e21a0f00e1b208b852052c89fe41cea1858f86a5
Doaa-Helal <doohelal055@gmail.com>
2025-03-02 00:39:37 +0200

    add data upload and processing endpoints with Pydantic request model

---
+++ src/routes/data.py
@@ -0,0 +1,74 @@
+from fastapi import FastAPI,APIRouter,Depends,UploadFile,status,Request
+from fastapi.responses import JSONResponse
+from .schemes.data import processRequest
+from helpers.config import get_settings,Settings
+from models.enums import ResponseSignal
+from models.ProjectModel import ProjectModel
+from controllers import DataController,ProjectController,ProcessController
+import aiofiles
+import logging
+import os
+
+logger=logging.getLogger("uvicorn.error")
+
+
+data_router=APIRouter(
+    prefix="/api/v1/data",
+    tags=["api_v1","data"]
+)
+
+@data_router.post("/upload/{project_id}")
+async def upload_data(request:Request,project_id:str,file:UploadFile,app_settings:Settings=Depends(get_settings)):
+    project_model=ProjectModel(request.app.db_client)
+    project=await project_model.get_project_or_create_one(project_id=project_id)
+
+    data_controller=DataController()
+    is_valid,signal=data_controller.validate_uploaded_file(file=file)
+
+    if not is_valid:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={"signal":ResponseSignal.FILE_UPLOADED_FAILED.value}
+        )
+    else:
+        project_dir_path=ProjectController().get_project_path(project_id=project_id)
+        file_path,file_id=data_controller.generate_unique_filepath(original_file_name=file.filename,project_id=project_id)
+
+        try:
+            async with aiofiles.open(file_path,"wb") as f:
+                while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                    await f.write(chunk)
+        except Exception as e:
+            logger.error(f"Error while uploading file: {str(e)}")
+            return JSONResponse(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                content={"signal":ResponseSignal.FILE_UPLOADED_FAILED.value})
+
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={"signal":ResponseSignal.FILE_UPLOADED_SUCCESS.value,
+                     "file_id":file_id,
+                 
+                     }
+        )
+    
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id:str,process_request:processRequest):
+    file_id=process_request.file_id
+    chunk_size=process_request.chunk_size
+    overlap_size=process_request.overlap
+
+    prosess_controller=ProcessController(project_id=project_id)
+    file_content=prosess_controller.get_file_content(file_id=file_id)
+    file_chunks=prosess_controller.process_file_content(file_content=file_content,file_id=file_id,chunk_size=chunk_size,overlap=overlap_size)
+    if file_chunks is None or len(file_chunks)==0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal":ResponseSignal.PROCESSING_FAILD.value
+            }
+        )
+    else:
+        
+     return file_chunks
+   
