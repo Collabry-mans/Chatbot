6d8979852c1ebc3444d437be3c618c8891ca739f
Doaa-Helal <doohelal055@gmail.com>
2025-04-11 15:30:54 +0200

    fix: make file_id optional in processRequest model

--- src/routes/schemes/data.py
+++ src/routes/schemes/data.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from typing import Optional
 
 class processRequest(BaseModel):
-    file_id:str
+    file_id:str=None
     chunk_size:Optional[int]=100
     overlap:Optional[int]=20
     do_rest:Optional[int]=0
