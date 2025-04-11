e154f236d2c73b60fa1916aac5e424770477d8a7
Doaa-Helal <doohelal055@gmail.com>
2025-04-11 15:29:06 +0200

    fix: handle non-existent file paths in get_file_loader and get_file_content methods

--- src/controllers/ProcessController.py
+++ src/controllers/ProcessController.py
@@ -19,6 +19,8 @@ class ProcessController(BaseController):
     def get_file_loader(self,file_id:str):
         file_ext=self.get_file_extension(file_id=file_id)
         file_path=os.path.join(self.project_path,file_id)
+        if not os.path.exists(file_path):
+            return None
         if file_ext==ProcessingEnum.TXT.value:
             return TextLoader(file_path=file_path,encoding="utf-8")
         
@@ -29,7 +31,9 @@ class ProcessController(BaseController):
     
     def get_file_content(self,file_id:str):
         loader=self.get_file_loader(file_id=file_id)
-        return loader.load()
+        if loader:
+             return loader.load()
+        return None
     
     def process_file_content(self,file_id:str,
                              file_content:list,chunk_size:int=100,overlap:int=20):
