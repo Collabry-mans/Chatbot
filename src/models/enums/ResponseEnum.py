de1ece0f8d358e50fe6edcd391a6895f1185cc97
Doaa-Helal <doohelal055@gmail.com>
2025-04-14 06:32:10 +0200

    fix: correct spelling errors in ResponseSignal enum and add new error signals

--- src/models/enums/ResponseEnum.py
+++ src/models/enums/ResponseEnum.py
@@ -13,3 +13,5 @@ class ResponseSignal(Enum):
     NO_FILES_ERROR="not_found_files"
     FILE_ID_ERROR="no_file_found_with_this_id"
     PROJECT_NOT_FOUNF_ERROR="project_not_found"
+    INSERT_INTO_VECTORDB_ERROR="insert_into_vector_db_error"
+    INSERT_INTO_VECTORDB_success="insert_into_vector_db_success"
