cdf5dd5d0b64a89f0f3699db6e4459e39f04a319
Doaa-Helal <doohelal055@gmail.com>
2025-04-20 07:14:51 +0200

    fix: remove unused index_project endpoint and update search_index response status to 200

--- src/routes/nlp.py
+++ src/routes/nlp.py
@@ -15,68 +15,6 @@ nlp_router=APIRouter(
     tags=["api_v1","nlp"]
 )
 
-@nlp_router.post("/index/push/{project_id}")
-async def index_project(request:Request,project_id:str,push_request:PushRequest):
-    
-    project_model= await ProjectModel.create_instance(
-        db_client=request.app.db_client
-    )
-
-    chunk_model= await ChunkModel.create_instance(
-        db_client=request.app.db_client
-    )
-    project=await project_model.get_project_or_create_one(
-        project_id=project_id
-    )
-    if not project:
-        return JSONResponse(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            content={
-                "signal":ResponseSignal.PROJECT_NOT_FOUND_ERROR.value
-            }
-        )
-    nlp_controller=NLPController(
-        vectordb_client=request.app.vectordb_client,
-        generation_client=request.app.generation_client,
-        embedding_client=request.app.embedding_client
-
-    )
-    has_records=True
-    page_number=1
-    inserted_items_count=0
-    idx=0
-    while has_records: 
-        page_chunks=await chunk_model.get_all_project_chunks(project_id=project.id,page_no=page_number)
-        if len(page_chunks):
-            page_number +=1
-        if not page_chunks or len(page_chunks)==0:
-            has_records=False
-            break
-        chucks_ids=list(range(idx,idx+len(page_chunks)))
-        idx+=len(page_chunks)
-
-        is_inserted=nlp_controller.index_into_vector_db(
-            project=project_model,
-            chunks=page_chunks,
-            do_reset=push_request.do_reset,
-            chunks_ids=chucks_ids
-        )
-
-        if not is_inserted:
-            return JSONResponse(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                content={
-                    "signal":ResponseSignal.INSERT_INTO_VECTORDB_ERROR.value
-                }
-            )
-        inserted_items_count+=len(page_chunks)
-    return JSONResponse(
-        content={
-                "signal":ResponseSignal.INSERT_INTO_VECTORDB_success.value,
-                "inserted_items_count":inserted_items_count
-        }
-    )
-
 @nlp_router.get("/index/info/{project_id}")
 async def get_project_index_info(request: Request, project_id: str):
     
@@ -135,6 +73,7 @@ async def search_index(request: Request, project_id: str, search_request: Search
             )
     
     return JSONResponse(
+        status_code=status.HTTP_200_OK,
         content={
             "signal": ResponseSignal.VECTORDB_SEARCH_SUCCESS.value,
             "results": [ result.dict()  for result in results ]
