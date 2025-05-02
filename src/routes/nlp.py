36fbbed478b8e9e6f62db3e0908f94463b7749da
Doaa-Helal <doohelal055@gmail.com>
2025-05-02 21:56:22 +0300

    fix: remove project parameter from get_vector_db_collection_info call

--- src/routes/nlp.py
+++ src/routes/nlp.py
@@ -32,7 +32,7 @@ async def get_project_index_info(request: Request, project_id: str):
         embedding_client=request.app.embedding_client,
     )
 
-    collection_info = nlp_controller.get_vector_db_collection_info(project=project)
+    collection_info = nlp_controller.get_vector_db_collection_info()
 
     return JSONResponse(
         content={
